c0a1c04e14478caf8de093efb968d0429b6d4ae6
João Pedro Telini <joaotelini03@gmail.com>
2025-03-19 21:13:05 -0300

    ft: risk task on complete

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,12 @@ function Tasks(props) {
     <ul className="space-y-2 p-6 bg-slate-200 rounded-md, shadow">
       {props.tasks.map((tasks) => (
         <li key={tasks.id} className="flex gap-2">
-          <button className="bg-slate-400 w-full text-white p-2 text-left rounded-md">
+          <button
+            onClick={() => props.onTaskClick(tasks.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              tasks.isCompleted ? "line-through" : ""
+            }`}
+          >
             {tasks.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
