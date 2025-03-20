bbb1313db1113429e003ee1b5452e4d760bc5657
João Pedro Telini <joaotelini03@gmail.com>
2025-03-19 21:03:48 -0300

    new function taskclick

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,21 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -33,7 +48,7 @@ function App() {
           Task Manangent
         </h1>
         <AddTasks />
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
       </div>
     </div>
   );
