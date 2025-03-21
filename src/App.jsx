5f45cd344c6883ff4432a0530222067aadff4c00
João Pedro Telini <joaotelini03@gmail.com>
2025-03-20 23:59:18 -0300

    fix spelling mistake

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
   return (
     <div className="w-full h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <Title>Task Manangent</Title>
+        <Title>Task Management</Title>
         <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
